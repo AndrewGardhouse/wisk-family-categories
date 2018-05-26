1530f96ecaaed84e0a9a95f71a66e7872be5c176
Andrew Gardhouse <andrewgardhouse@Andrews-MacBook-Air.local>
2018-05-26 10:33:06 -0500

    Families modal pulls in categories-family json

--- src/components/FamiliesModal.vue
+++ src/components/FamiliesModal.vue
@@ -1,11 +1,25 @@
 <template>
-  <b-modal id="familiesModal" title="Families Modal">
-    <p class="my-4">Families Modal</p>
-  </b-modal>
+  <b-modal id="familiesModal" title="Families Modal"></b-modal>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data() {
+    return {
+      families: []
+    }
+  },
+  created() {
+    axios.get('./static/categories-families.json')
+      .then((res) => {
+        this.families = res.data.data
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
 }
 </script>
 
