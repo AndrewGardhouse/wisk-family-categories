a5cbd2f60030b8e99ad7dbb5b26fcb2bc1119d56
Andrew Gardhouse <andrewgardhouse@Andrews-MacBook-Air.local>
2018-05-26 09:26:17 -0500

    App component uses CategoryModal

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <h1>Wisk Solutions</h1>
     <Dropdown></Dropdown>
+    <CategoryModal></CategoryModal>
   </div>
 </template>
 
 <script>
 import Dropdown from './components/Dropdown'
+import CategoryModal from './components/CategoryModal'
 
 export default {
   name: 'app',
   components: {
-    Dropdown
+    Dropdown,
+    CategoryModal
   }
 }
 </script>
