8168d08e61e89f03d1083776b5975dfcb7f40d4e
Andrew Gardhouse <andrewgardhouse@Andrews-MacBook-Air.local>
2018-05-25 22:31:58 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Wisk Solutions</h1>
+    <Dropdown></Dropdown>
+  </div>
+</template>
+
+<script>
+import Dropdown from './components/Dropdown'
+
+export default {
+  name: 'app',
+  components: {
+    Dropdown
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-dropdown id="actions" text="Actions" class="m-md-2">
+      <b-dropdown-item>
+        <span v-b-modal="'modal1'">
+          Manage Families and Categories
+        </span>
+      </b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
